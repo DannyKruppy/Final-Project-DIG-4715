e4b8fac7e27d308a0634f1381e3126a192dad40d
Danny <dlkrupczak@gmail.com>
2026-04-04 03:01:10 -0400

    Framework - Main Menu
    Added a functioning main menu
    complete with scenes for:
    lore - complete
    controls
    credits
    a sample game scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+     public void Lore()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void BacktoMain()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+   
+
+
+    
+}
