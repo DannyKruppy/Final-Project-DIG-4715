29d52618ae2b64d858e270a6c74149088a74a435
nominal251 <jackhardy524@gmail.com>
2026-04-12 04:16:28 -0400

    Implement time switch meter

--- Assets/Scripts/TimeSwitch.cs
+++ Assets/Scripts/TimeSwitch.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
 
 public class TimeSwitch : MonoBehaviour
 {
@@ -14,10 +15,30 @@ public class TimeSwitch : MonoBehaviour
     public PauseScript pauseManager;
 
     bool isPast = true;
+    bool timeLocked = false;
+
+    public float maxTime = 8f;
+    public float drainRate = 1f;
+    public float rechargeRate = 0.5f;
+    public float timeDelay = 2f;
+
+    private float time;
+    private float counter;
+
+    public GameObject timeBar;
+    private Image timeBarImage;
+
+    Color blueColor;
+    Color redColor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        timeBarImage = timeBar.GetComponent<Image>();
+        time = maxTime;
+        blueColor = timeBarImage.color;
+        redColor = new Color(1f, 0f, 0f, 0.5f);
+
         foreach (GameObject obj in futureObjects)
         {
             obj.SetActive(false);
@@ -36,32 +57,83 @@ public class TimeSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Keyboard.current.fKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame) && pauseManager.paused == false)
+        if ((Keyboard.current.fKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame) && pauseManager.paused == false && timeLocked == false)
         {
             isPast = !isPast;
-
-            foreach (GameObject obj in pastObjects)
-            {
-                obj.SetActive(isPast);
-                SetMat(obj);
-            }
-
-            foreach (GameObject obj in futureObjects)
-            {
-                obj.SetActive(!isPast);
-                SetMat(obj);
-            }
-
-            foreach (GameObject obj in persistentObjects)
-            {
-                SetMat(obj);
-            }
+            ApplyState();
         }
+
+        TimeFrame();
+
+        if (time <= 0)
+            timeLocked = true;
+        else if (time >= maxTime / 2f)
+            timeLocked = false;
+
+        if (timeLocked == true)
+            timeBarImage.color = redColor;
+        else
+            timeBarImage.color = blueColor;
+
+        Recharge();
+
+        timeBarImage.fillAmount = time / maxTime;
     }
 
     void SetMat(GameObject obj)
     {
         Renderer renderer = obj.GetComponent<Renderer>();
         renderer.sharedMaterial = isPast ? pastMat : futureMat;
+    }
+
+    void TimeFrame()
+    {
+        if (!isPast && time > 0)
+        {
+            time -= drainRate * Time.deltaTime;
+            counter = 0;
+        }
+
+        if (time <= 0 && !isPast)
+        {
+            isPast = true;
+            ApplyState();
+        }
+    }
+
+    void Recharge()
+    {
+        if (time >= maxTime)
+        {
+            time = maxTime;
+            return;
+        }
+
+        counter += Time.deltaTime;
+
+        if (counter >= timeDelay)
+        {
+            time += rechargeRate * Time.deltaTime;
+        }
+    }
+
+    void ApplyState()
+    {
+        foreach (GameObject obj in pastObjects)
+        {
+            obj.SetActive(isPast);
+            SetMat(obj);
+        }
+
+        foreach (GameObject obj in futureObjects)
+        {
+            obj.SetActive(!isPast);
+            SetMat(obj);
+        }
+
+        foreach (GameObject obj in persistentObjects)
+        {
+            SetMat(obj);
+        }
     }
 }
